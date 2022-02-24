6d77e5a789f4264205c1e81ebe9041c5065bd013
MTY777 <mataymekkurmanbek@gmail.com>
2022-02-24 19:22:18 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework13mounth/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.homework13mounth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
